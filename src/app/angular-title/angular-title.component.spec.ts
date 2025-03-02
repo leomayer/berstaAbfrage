9e36e2a5d1d97bad8b6551324399f9e595cf38a9
leomayer <leo.zen88@gmail.com>
2025-03-02 18:03:47 +0100

    Initial commit

---
+++ src/app/angular-title/angular-title.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AngularTitleComponent } from './angular-title.component';
+
+describe('AngularTitleComponent', () => {
+  let component: AngularTitleComponent;
+  let fixture: ComponentFixture<AngularTitleComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AngularTitleComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AngularTitleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
